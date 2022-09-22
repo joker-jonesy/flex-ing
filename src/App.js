6eb22b5c06dd9283d1578792a059c8bc6cdb1114
joker-jonesy <spashmcgrath94@yahoo.com>
2022-09-22 14:48:41 -0400

    Kermit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flexOne">
+        <div className="item one">1</div>
+          <div className="container">
+              <div className="item two">2</div>
+              <div className="item three">3</div>
+          </div>
+
+      </div>
     </div>
   );
 }
